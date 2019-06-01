0b9ab7b7853d2b0a67aded6dcf909a7ac1329c5d
whosAle <abelgrave95@gmail.com>
2019-06-01 12:47:03 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,10 +2,10 @@ export function manageFriends(state, action){
   switch (action.type) {
     case "ADD_FRIEND":
 
-      return {...state, friends: [...state.friends, action.friend]} ;
+      return {friends: [...state.friends, action.friend]} ;
     case "REMOVE_FRIEND":
 
-      return {...state, friends: state.friends.filter(friend => friend.id !== action.id)} ;
+      return {friends: state.friends.filter(friend => friend.id !== action.id)} ;
     default:
       return state;
   }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,7 @@
 export function managePresents(state, action){
   switch (action.type) {
     case "INCREASE":
-      return {...state, numberOfPresents: state.numberOfPresents + 1}
+      return {numberOfPresents: state.numberOfPresents + 1}
     default:
       return state
   }
